30f59377ca3a5f9201517296b4109a45a6591ff7
tristanldavis2019-cmyk <tristanldavis2019@gmail.com>
2025-09-24 00:49:20 -0400

    adding

--- Lab 1/3.3.php
+++ Lab 1/3.3.php
@@ -6,7 +6,10 @@
     <body>
         <h1>ENTER YOUR INFO PLEASE </h1>
         <form action="3.3.info.php" method="get">
-            <input type="text" id="name">
+            <p><label for="name"> Name:</label><input type="text" id="name"></p>
+            <p> <label for="animal">Favorite animal:</label><input type="text" id="animal"> </p>
+            <p> <label for="food">Favorite food:</label> <input type="text" id="food"> </p>
+            <p> <label for="WW2">Favorite year of WW2</label> <input type="text" id="WW2"> </p>
             <input type="submit" value="submit">
         </form>
     </body>
