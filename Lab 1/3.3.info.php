d980f08f2e1fba906179094002325471dd2a9e36
tristanldavis2019-cmyk <tristanldavis2019@gmail.com>
2025-09-24 01:18:25 -0400

    making it user friendly
    some polite critique

--- Lab 1/3.3.info.php
+++ Lab 1/3.3.info.php
@@ -4,7 +4,15 @@
         <title> 3.3.info.php </title>    
     </head>
     <body>
-        <p>GET: <pre><?= var_dump($_GET) ?></pre></p>
-        <p>POST: <pre><?= var_dump($_POST) ?></pre></p>
+        <p><?= var_dump($_GET) ?></p>
+        <p><?= var_dump($_POST) ?></p>
+        <div> 
+            So <?php echo htmlspecialchars($_GET['name']) ?>, 
+            I hear you like <?php echo htmlspecialchars($_GET['animal']) ?> and
+            <?php echo htmlspecialchars($_GET['food']) ?>, why did you answer that 
+            WW2 question though? 
+            <?php echo (int)$_GET['WW2'] ?>, really?
+            Kind of weird... 
+        </div>
     </body>
 </html>
--- Lab 1/3.3.php
+++ Lab 1/3.3.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <h1>ENTER YOUR INFO PLEASE </h1>
-        <form action="3.3.info.php" method="post">
+        <form action="3.3.info.php" method="get">
             <p><label for="name"> Name:</label><input type="text" id="name" name="name"></p>
             <p> <label for="animal">Favorite animal:</label><input type="text" id="animal" name="animal"> </p>
             <p> <label for="food">Favorite food:</label> <input type="text" id="food" name= "food"> </p>
