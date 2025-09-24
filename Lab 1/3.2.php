df62bdbf26d8739a9378f3d839fac534095c41fe
tristanldavis2019-cmyk <tristanldavis2019@gmail.com>
2025-09-23 23:37:42 -0400

    uas geting
    fist attempt to get user agent string

--- Lab 1/3.2.php
+++ Lab 1/3.2.php
@@ -4,6 +4,7 @@
         <title> 3.2.php </title>    
     </head>
     <body>
-        <p><?php var_dump($_SERVER); ?></p>
+        <h1>YOUR USER AGENT STRING IS: </h1>
+        <p><?php echo "$_SERVER['HTTP_USER_AGENT']"; ?></p>
     </body>
 </html>
