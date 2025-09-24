5e82d875a58209ab8bec50a5368031d74f478adb
tristanldavis2019-cmyk <tristanldavis2019@gmail.com>
2025-09-24 00:25:26 -0400

    made the pair of pages
    basic get and response page

---
+++ Lab 1/3.3.info.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 3.3.info.php </title>    
+    </head>
+    <body>
+        <p>GET: <?= var_dump($_GET) ?></p>
+        <p>POST: <?= var_dump($_POST) ?></p>
+    </body>
+</html>
---
+++ Lab 1/3.3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 3.3.php </title>    
+    </head>
+    <body>
+        <h1>ENTER YOUR INFO PLEASE </h1>
+        <form action="3.3.info.php" method="get">
+            <input type="text" id="name">
+            <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
