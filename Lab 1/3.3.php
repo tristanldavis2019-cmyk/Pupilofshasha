078d784001e42c31b59788ce545906f0ef044cd9
tristanldavis2019-cmyk <tristanldavis2019@gmail.com>
2025-09-24 00:52:55 -0400

    names
    apparently you need names to transmit data to the server. who knew

--- Lab 1/3.3.php
+++ Lab 1/3.3.php
@@ -6,10 +6,10 @@
     <body>
         <h1>ENTER YOUR INFO PLEASE </h1>
         <form action="3.3.info.php" method="get">
-            <p><label for="name"> Name:</label><input type="text" id="name"></p>
-            <p> <label for="animal">Favorite animal:</label><input type="text" id="animal"> </p>
-            <p> <label for="food">Favorite food:</label> <input type="text" id="food"> </p>
-            <p> <label for="WW2">Favorite year of WW2</label> <input type="text" id="WW2"> </p>
+            <p><label for="name"> Name:</label><input type="text" id="name" name="name"></p>
+            <p> <label for="animal">Favorite animal:</label><input type="text" id="animal" name="animal"> </p>
+            <p> <label for="food">Favorite food:</label> <input type="text" id="food" name= "food"> </p>
+            <p> <label for="WW2">Favorite year of WW2</label> <input type="text" id="WW2" name= "WW2"></p>
             <input type="submit" value="submit">
         </form>
     </body>
