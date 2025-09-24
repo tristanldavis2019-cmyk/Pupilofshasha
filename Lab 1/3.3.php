04302496f6ca64f621b1aafdfc9cef7d7aaa8d11
tristanldavis2019-cmyk <tristanldavis2019@gmail.com>
2025-09-24 00:54:19 -0400

    Update 3.3.php

--- Lab 1/3.3.php
+++ Lab 1/3.3.php
@@ -9,7 +9,7 @@
             <p><label for="name"> Name:</label><input type="text" id="name" name="name"></p>
             <p> <label for="animal">Favorite animal:</label><input type="text" id="animal" name="animal"> </p>
             <p> <label for="food">Favorite food:</label> <input type="text" id="food" name= "food"> </p>
-            <p> <label for="WW2">Favorite year of WW2</label> <input type="text" id="WW2" name= "WW2"></p>
+            <p> <label for="WW2">Favorite year of WW2: </label> <input type="text" id="WW2" name= "WW2"></p>
             <input type="submit" value="submit">
         </form>
     </body>
