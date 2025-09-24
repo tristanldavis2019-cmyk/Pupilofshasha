ac8f5caabc51f9b5a077ae69d437b18642aa8616
tristanldavis2019-cmyk <tristanldavis2019@gmail.com>
2025-09-24 00:36:31 -0400

    trying to print
    adding the <pre> tags to see what the dump is printing

--- Lab 1/3.3.info.php
+++ Lab 1/3.3.info.php
@@ -4,7 +4,7 @@
         <title> 3.3.info.php </title>    
     </head>
     <body>
-        <p>GET: <?= var_dump($_GET) ?></p>
-        <p>POST: <?= var_dump($_POST) ?></p>
+        <p>GET: <pre><?= var_dump($_GET) ?></pre></p>
+        <p>POST: <pre><?= var_dump($_POST) ?></pre></p>
     </body>
 </html>
